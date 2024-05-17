dd1330d5cc46955a39f5575fd31ff6ba7725812b
Paul Cortes <pcorqui@gmail.com>
2024-05-17 17:41:06 -0600

    Initial commit

---
+++ src/main/java/com/paul/SGCyA/SgCyAApplication.java
@@ -0,0 +1,13 @@
+package com.paul.SGCyA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgCyAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgCyAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paul/SGCyA/SgCyAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.SGCyA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgCyAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
