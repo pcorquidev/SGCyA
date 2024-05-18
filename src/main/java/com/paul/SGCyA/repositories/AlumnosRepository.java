da8d2973e37ac8f8c8f76748acb148c22dd32557
Paul Cortes <pcorqui@gmail.com>
2024-05-17 19:25:42 -0600

    feat(Repository): agregadas interfaces repository

---
+++ src/main/java/com/paul/SGCyA/repositories/AlumnosRepository.java
@@ -0,0 +1,7 @@
+package com.paul.SGCyA.repositories;
+
+import com.paul.SGCyA.models.Alumno;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlumnosRepository extends CrudRepository<Alumno,Long> {
+}
---
+++ src/main/java/com/paul/SGCyA/repositories/CursosRespository.java
@@ -0,0 +1,7 @@
+package com.paul.SGCyA.repositories;
+
+import com.paul.SGCyA.models.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursosRespository extends CrudRepository<Curso, Long> {
+}
